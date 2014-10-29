363a18918d6dabc0e731344d655406850b6c5e57
Laurent Gauthier <Laurent Gauthier@localhost>
2014-10-29 11:37:30 -0400

    some history writing functions for calib.py

--- lib/tools_write.py
+++ lib/tools_write.py
@@ -88,6 +88,51 @@ def findCalibName(dirname):
     filename = 'point' + str(last_num)
     
     return filename
+
+################################ 
+#        Calibration history functions       
+################################
+def read_history():
+    '''finds the number of the last recorded evaluation and returns the number of the new evaluation'''
+    last_num = 0
+    with open('.\history.txt', 'r') as hist:
+        for l in hist:
+            if l.strip() != '' and l.startswith('#') is False:
+                last_num += 1
+    return last_num + 1
+
+def create_history(dirname, filename, networks):
+    with open(os.path.join(dirname, filename), 'r') as hist:
+        hist.write('Itt\t|\tpoint\t|\t')
+        for net in xrange(len(networks)):
+            for comp in xrange(len(networks[net].videoComparison)):
+                hist.write('Network_'+str(net)+'Video_'+str(comp)+'|\t')
+		
+        hist.write('fout\n')		
+	
+def write_history(last_num, points, networks, fout,  dirname, filename):
+    with open(os.path.join(dirname, filename), 'a') as hist:
+        hist.write(str(last_num)+'\t')
+        hist.write("|\t")
+
+        #tried point
+        for p in points:
+            hist.write(str(p)+'\t')
+            hist.write("|\t")
+
+        #secondary comparaison
+        for net in xrange(len(networks)):
+            for comp in networks[net].videoComparison:
+                variables = writeToOneList(list(comp))
+                for var in variables:
+                    if isinstance(var,float) is True:
+                        hist.write(str(round(var,4)) + '\t')
+                    else:
+                        hist.write(str(var) +'\t')
+                hist.write("|\t")
+
+        #fout
+        hist.write(str(fout)+'\n')
     
 ##################
 # Report tools
