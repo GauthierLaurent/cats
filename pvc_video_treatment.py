Fixed the calculation of speed which was taking the alignment number in the calculation

pvc_video_treatment.py
@@ -244,7 +244,7 @@ def turnSqliteIntoTraj(config, min_time, max_time, fps, video_list, diagnosis, m
                     o.curvilinearVelocities = o.curvilinearPositions.differentiate(True)
                     
                     if diagnosis is True:
-                        if np.mean(np.asarray(o.curvilinearVelocities)) > maxSpeed:
+                        if np.mean(np.asarray(o.curvilinearVelocities)[:,0]) > maxSpeed:
                             excess_speed.append(o)
                         if np.mean(np.asarray(o.curvilinearVelocities)) < 0:
                             invert_speed.append(o)

python/pvc_write.py
@@ -499,7 +499,7 @@ def writeDisgnosedReport(dirname, filename, video_names, inpxname, min_time, max
     out.write('Speed threshold used: '+str(maxSpeed*3.6*fps)+'\n')
     out.write('List of objects with speed greater than threshold: (#obj, mean speed)\n')
     for exc in excess_speed:
-        writeInFile(out, [exc.getNum(), np.mean(np.asarray(exc.curvilinearVelocities)*fps*3.6)] )
+        writeInFile(out, [exc.getNum(), np.mean(np.asarray(exc.curvilinearVelocities)[:,0])*fps*3.6] )
     for inv in invert_speed:
         writeInFile(out, [inv.getNum(), np.mean(np.asarray(inv.curvilinearVelocities))] )
           