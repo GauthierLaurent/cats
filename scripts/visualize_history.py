fdd65e32a7e5a1a8122222e31193d5631c26175d
Laurent Gauthier <Laurent Gauthier@localhost>
2015-03-03 08:21:17 -0500

    The wrong file had been commited in the last commit, here is the true vizualize update commit. The changes to compare-dist where of deveoppement nature.

--- scripts/visualize_history.py
+++ scripts/visualize_history.py
@@ -4,25 +4,36 @@ Created on Thu Feb 19 18:45:28 2015
 
 @author: Laurent
 """
-
+#External librairies
 import os, argparse
 import matplotlib.pyplot as plt
 
-import pvc_write  as write
-import pvc_define as define
+#Internal libraries
+import pvc_write   as write
+import pvc_define  as define
+import pvc_outputs as outputs
 
+#Command parser
 def commands(parser):
-    parser.add_argument('--nbr',    type=int, dest='nbr',    default=2,     help='Number of graphs per figure (Nx1)') 
-    parser.add_argument('--hspace',           dest='hspace', default=0.3,   help='Horizontal space between subplots')
-    parser.add_argument('--csv',              dest='csv',    required=True, help='Directory (Optional: default is current working directory)')
+    parser.add_argument('--nbr',    type=int,   dest='nbr',    default=2,     help='Number of graphs per figure (Nx1)') 
+    parser.add_argument('--hspace',             dest='hspace', default=0.3,   help='Horizontal space between subplots')
+    parser.add_argument('--csv',                dest='csv',    required=True, help='Directory (Optional: default is current working directory)')
+    parser.add_argument('--nVeh',   type=float, dest='num',    default=10)
+    parser.add_argument('--decel',  type=float, dest='dp',     default=0)
+    parser.add_argument('--accel',  type=float, dest='a0',     default=0)
     return parser.parse_args()
-    
-Commands = commands(argparse.ArgumentParser())
 
+class thisConfig:
+    def __init__(self,num,dp,a0):
+        self.num_const_thresh = num
+        self.dp_const_thresh  = dp
+        self.a0_const_thresh  = a0
+
+#Main code
+Commands = commands(argparse.ArgumentParser())
 a = Commands.nbr
 
 history = write.History.read_history('calib_history.txt')
-
 all_variables = define.extractParamFromCSV(os.curdir,Commands.csv)
 variables = [var for var in all_variables if var.include is True]
 chunks = define.cleanChunks(a, variables)
@@ -34,11 +45,26 @@ for c in xrange(len(chunks)):
             ax[-(axis+1)].axis('off')
 
     for v in xrange(len(chunks[c])):
-        x = [p.point[a*c+v] for p in history if p.fout != 'crashed']
-        y = [p.fout for p in history if p.fout != 'crashed']
+
+        #without the presence of error files
+        x_b = []
+        y_b = []
+        #with the presence of at least an error file
+        x_r = []        
+        y_r = []
         
-            #import pdb;pdb.set_trace()
-        ax[v].plot(x,y, linestyle='None', marker='o')
+        for p in history:
+            if p.fout != 'crashed':
+                if p.C_0 > 0 or p.C_1 > 0 or p.C_2 > 0:
+                    x_r.append(p.point[a*c+v])
+                    y_r.append(p.fout)
+                    
+                else:
+                    x_b.append(p.point[a*c+v])
+                    y_b.append(p.fout)
+        
+        ax[v].plot(x_b,y_b, linestyle='None', marker='o', color = 'b')
+        ax[v].plot(x_r,y_r, linestyle='None', marker='o', color = 'r')
         ax[v].set_title('Value of fout vs Value of ' + variables[a*c+v].name)
 
     plt.subplots_adjust(hspace=Commands.hspace)
